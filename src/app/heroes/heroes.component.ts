2d79d76bfa13406e0243300ac75865758d4f1c4d
vangaru <ivanenko0607@gmail.com>
2021-10-18 16:55:09 +0300

    feat: List Of Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from "../Hero";
+import { Hero } from "../hero";
+import { HEROES } from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,13 @@ import {Hero} from "../Hero";
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelectHero(hero: Hero) {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
